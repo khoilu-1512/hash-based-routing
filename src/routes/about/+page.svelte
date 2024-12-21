d49c9506cff6ad2098dfd7b9cf275cac3145ffdb
Rich Harris <rich.harris@vercel.com>
2024-12-21 12:31:46 -0500

    convert to hash-based routing

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,20 +7,20 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
-		the following into your command line and following the prompts:
-	</p>
-
-	<pre>npx sv create</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		This is the default <a href="https://svelte.dev/docs/kit">SvelteKit</a> demo app, but with hash-based
+		routing — instead of determining the current route from the URL pathname, SvelteKit determines it
+		from the hash.
 	</p>
 
 	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
+		This is useful in situations where (for example) you don't control the webserver where your app
+		is deployed, and can't specify a <code>200.html</code> fallback page or similar. It's also useful
+		in the context of things like Electron apps.
+	</p>
+
+	<p>
+		It comes with some caveats: you can't use server-side rendering (or indeed any server logic),
+		and you have to make sure that the links in your app all start with <code>/#/</code>, or they
+		won't work. Beyond that, everything works exactly like a normal SvelteKit app.
 	</p>
 </div>
